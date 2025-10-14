63da9a14ffd34ee0e28f7cf72c40b868eeb05709
yaroslavn <yaroslav.n@open-tag.com>
2025-10-14 21:48:00 +0300

    menu active: created dropdown menu for category khif.

--- js/header/header.js
+++ js/header/header.js
@@ -1,9 +1,10 @@
 
   const menuItems = document.querySelectorAll('.link-active');
+  const dropdownMenu = document.querySelector('.menu__dropdown');
 
   menuItems.forEach(item => {
     item.addEventListener('click', () => {
-      menuItems.forEach(el => el.classList.remove('active'));
-      item.classList.add('active');
+      menuItems.forEach(el => el.classList.remove('active-menu'));
+      dropdownMenu.classList.toggle('active-menu');
     });
   });
